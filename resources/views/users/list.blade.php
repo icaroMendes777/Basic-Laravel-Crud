38290eb4d8e5c6fc9ab372419b2131d012b269c0
icaroMendes777 <114268516+icaroMendes777@users.noreply.github.com>
2023-03-25 10:17:56 -0300

    Initial commit

---
+++ resources/views/users/list.blade.php
@@ -0,0 +1,36 @@
+
+
+@include('users.header', ['page' => 'list'])
+
+
+<h3 class="m-4 text-info">Total: {{$total}}</h3>
+
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Email</th>
+        <th scope="col">Detalhes</th>
+        <th scope="col">Deletar</th>
+      </tr>
+    </thead>
+    <tbody>
+
+    @foreach($users as $u)
+        <tr>
+            <th scope="row">{{$u->id}}</th>
+            <td>{{$u->name}}</td>
+            <td>{{$u->email}}</td>
+            <td><a href="{{ URL::route('user', $u->id) }}" class="btn btn-primary">Ver</a></td>
+            <td><a href="{{ URL::route('user.delete', $u->id) }}" class="btn btn-danger">Deletar</a></td>
+        </tr>
+    @endforeach
+
+    </tbody>
+  </table>
+
+
+  @include('users.footer')
+
+</div>
---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return redirect('/users');
+});
+
+
+Route::get('/users', [Users::class, 'index'])->name('list');
+
+Route::get('/users/{id}', [Users::class, 'show'])->name('user');
+
+Route::get('/users/{id}/delete', [Users::class, 'destroy'])->name('user.delete');
+
+//Route::get('/users/sobre', [Users::class, 'user'])->name('sobre');
+
+Route::view('sobre', '/users/sobre')->name('sobre');
